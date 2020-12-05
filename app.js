899303405f7297fa52842c7452e4a9cb2b29f164
Ilton Andrew <iltonandrew@gmail.com>
2020-12-05 02:53:13 -0300

    init whatsauto integration

--- app.js
+++ app.js
@@ -7,47 +7,47 @@ const productRoute = require('./routes/product-route');
 const categoryRoute = require('./routes/category-route');
 const orderRoute = require('./routes/order-route');
 const userRoute = require('./routes/user-route');
-const imageRoute  = require('./routes/image-route');
+const imageRoute = require('./routes/image-route');
 
 app.use(morgan('dev'));
 app.use('/uploads', express.static('uploads'));
-app.use(bodyParser.urlencoded({ extended: false }));  // apenas dados simples
+app.use(bodyParser.urlencoded({ extended: false })); // apenas dados simples
 app.use(bodyParser.json()); // json de entrada no body
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Header',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).send({});
-    }
-    next();
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).send({});
+  }
+  next();
+});
+app.get('', (request, response) => {
+  return response.json('Salve');
 });
 
-app.use('/products', productRoute);
-app.use('/categories', categoryRoute);
-app.use('/orders', orderRoute);
-app.use('/users', userRoute);
-app.use('/images', imageRoute);
+app.post('', (resquest, response) => {
+  const { sender, message } = resquest.body;
+
+  return response.json({ reply: `Olá ${sender}, sua mensagem foi: ${message}` });
+});
 
 // Quando não encontra rota, entra aqui:
 app.use((req, res, next) => {
-    const erro = new Error('Não encontrado');
-    erro.status = 404;
-    next(erro);
+  const erro = new Error('Não encontrado');
+  erro.status = 404;
+  next(erro);
 });
 
 app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    return res.send({
-        erro: {
-            mensagem: error.message
-        }
-    });
+  res.status(error.status || 500);
+  return res.send({
+    erro: {
+      mensagem: error.message,
+    },
+  });
 });
 
 module.exports = app;
