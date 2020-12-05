bfc281993b0170a3ff845004744ba5d4cfc0676f
Ilton Andrew <iltonandrew@gmail.com>
2020-12-05 02:37:32 -0300

    add procfile

--- routes/user-route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const userController = require('../controllers/user-controller');
-
-router.post('/', userController.createUser);
-router.post('/login', userController.Login)
-
-module.exports = router;
